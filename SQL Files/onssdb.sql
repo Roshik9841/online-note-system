f820f6a5a13527ced74fe369a06829328f61cd32
I am Roshik <Roshik9841@gmail.com>
2024-06-13 23:59:01 +0545

    Update onssdb.sql

--- SQL Files/onssdb.sql
+++ SQL Files/onssdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2023 at 07:07 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Jun 03, 2024 at 04:53 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -46,13 +46,10 @@ CREATE TABLE `tblnotes` (
 --
 
 INSERT INTO `tblnotes` (`ID`, `UserID`, `Subject`, `NotesTitle`, `NotesDecription`, `File1`, `File2`, `File3`, `File4`, `CreationDate`, `UpdationDate`) VALUES
-(1, 3, 'Math', 'Maths Shortcuts', 'It contain math shortcuts.', 'd41d8cd98f00b204e9800998ecf8427e1702536045.pdf', 'd41d8cd98f00b204e9800998ecf8427e1702536260', 'd41d8cd98f00b204e9800998ecf8427e1702536700', 'd41d8cd98f00b204e9800998ecf8427e1702534796.pdf', '2023-12-14 06:19:56', '2023-12-14 06:51:40'),
-(2, 3, 'English', 'English Vocabulary', 'shgfgrhfgrw\r\nfdfhreiufyhw\r\nfewyhiufywe', 'd41d8cd98f00b204e9800998ecf8427e1702539232.pdf', 'd41d8cd98f00b204e9800998ecf8427e1702539232.pdf', 'd41d8cd98f00b204e9800998ecf8427e1702539232.pdf', '', '2023-12-14 07:33:52', NULL),
-(3, 3, 'English', 'English Literature', 'shgfgrhfgrw\r\nfdfhreiufyhw\r\nfewyhiufywe', 'd41d8cd98f00b204e9800998ecf8427e1702539232.pdf', 'd41d8cd98f00b204e9800998ecf8427e1702539232.pdf', 'd41d8cd98f00b204e9800998ecf8427e1702539232.pdf', '', '2023-12-14 07:33:52', '2023-12-14 13:30:34'),
-(4, 2, 'Math', 'Maths Shortcuts', 'It contain math shortcuts.', 'd41d8cd98f00b204e9800998ecf8427e1702536045.pdf', 'd41d8cd98f00b204e9800998ecf8427e1702536260', 'd41d8cd98f00b204e9800998ecf8427e1702536700', 'd41d8cd98f00b204e9800998ecf8427e1702534796.pdf', '2023-12-14 06:19:56', '2023-12-14 13:18:41'),
-(5, 1, 'Math', 'Maths Shortcuts', 'It contain math shortcuts.', 'd41d8cd98f00b204e9800998ecf8427e1702536045.pdf', 'd41d8cd98f00b204e9800998ecf8427e1702536260', 'd41d8cd98f00b204e9800998ecf8427e1702536700', 'd41d8cd98f00b204e9800998ecf8427e1702534796.pdf', '2023-12-14 06:19:56', '2023-12-14 13:18:46'),
-(6, 2, 'English', 'English Vocabulary', 'shgfgrhfgrw\r\nfdfhreiufyhw\r\nfewyhiufywe', 'd41d8cd98f00b204e9800998ecf8427e1702539232.pdf', 'd41d8cd98f00b204e9800998ecf8427e1702539232.pdf', 'd41d8cd98f00b204e9800998ecf8427e1702539232.pdf', '', '2023-12-14 07:33:52', '2023-12-14 13:18:51'),
-(8, 4, 'PHP', 'PHP PDO, Array and Strings', 'PHP Data Object-PDO, Array and Strings', 'd41d8cd98f00b204e9800998ecf8427e1702662450.pdf', 'd41d8cd98f00b204e9800998ecf8427e1702662450.pdf', '', '', '2023-12-15 17:47:30', '2023-12-15 17:47:55');
+(10, 5, 'SAD', 'unit 1', 'exam lai padh ', 'd41d8cd98f00b204e9800998ecf8427e1717257202.pdf', 'd41d8cd98f00b204e9800998ecf8427e1717257202.pdf', '', '', '2024-06-01 15:53:22', NULL),
+(11, 5, 'Java', 'String handling', 'thikxa thikxa\r\n', 'd41d8cd98f00b204e9800998ecf8427e1717257339.pdf', '', '', '', '2024-06-01 15:55:39', NULL),
+(12, 5, 'Web development', 'Web inspector', 'Hyaa yo sir ta kaam nai xaina', 'd41d8cd98f00b204e9800998ecf8427e1717257504.pdf', '', '', '', '2024-06-01 15:58:24', NULL),
+(13, 5, 'tha xaina', 'Tha xaina', 'tha xaina', 'd41d8cd98f00b204e9800998ecf8427e1717257534.pdf', '', '', '', '2024-06-01 15:58:54', NULL);
 
 -- --------------------------------------------------------
 
@@ -74,10 +71,7 @@ CREATE TABLE `tbluser` (
 --
 
 INSERT INTO `tbluser` (`ID`, `FullName`, `MobileNumber`, `Email`, `Password`, `RegDate`) VALUES
-(1, 'Abir Singh', 9798789789, 'abir@gmail.com', '202cb962ac59075b964b07152d234b70', '2022-06-06 13:36:36'),
-(2, 'Anuj Kumar', 1425362514, 'ak@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2022-06-11 11:48:57'),
-(3, 'Raghav', 7897979878, 'rahgav@gmail.com', '202cb962ac59075b964b07152d234b70', '2023-12-14 05:26:12'),
-(4, 'John Doe', 1122112211, 'john12@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2023-12-15 17:46:20');
+(5, 'Roshik Maharjan', 9843225292, 'Roshik9841@gmail.com', 'eec6292c1e734e8b499c1a4d2d2f2d05', '2024-06-01 15:52:10');
 
 --
 -- Indexes for dumped tables
@@ -103,13 +97,13 @@ ALTER TABLE `tbluser`
 -- AUTO_INCREMENT for table `tblnotes`
 --
 ALTER TABLE `tblnotes`
-  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbluser`
 --
 ALTER TABLE `tbluser`
-  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
